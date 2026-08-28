df8b3bba78416ee5d81932b213cbdc2c78eee5d3
AgungADL <agungcoygamez123@gmail.com>
2026-08-28 15:18:50 +0700

    feat: create learning hub welcome page

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- Bawaan Nuxt -->
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+
+    <!-- Mulai Belajar nya -->
+    <h1>Learning Hub</h1>
+    <p>Welcome back, Agung!</p>
+    <p>Your journey starts here.</p>
+    <button>Starts Learning</button>
+
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
